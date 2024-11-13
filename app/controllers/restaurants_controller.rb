9356820e30d38e64c99f3d647087ce9038884cbc
Thomas Alonso <thomas.alonso93@gmail.com>
2024-11-13 16:11:28 +0100

    my project

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,15 +1,30 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
 
-def index
-end
+  def index
+    @restaurants = Restaurant.all
+  end
 
-def new
-end
+  def new
+    @restaurant = Restaurant.new
+  end
 
-def create
-end
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to restaurants_path
+  end
 
-def show
-end
+  def show
+  end
 
+ private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
